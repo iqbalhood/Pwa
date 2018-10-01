eb61200ac9f40b345e06e772baadebeb6e2f9e72
millea <millea1994@gmail.com>
2018-10-01 22:23:39 +0700

    implement cache then network

--- sw.js
+++ sw.js
@@ -32,14 +32,28 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+  var request = event.request;
+  var url     = new URL(request.url);
+  //Separate response if cache from internal 
+  if(url.origin === location.origin){
+      event.respondWith(
+        caches.match(request).then(function(response){
+          return response || fetch(request)
+        })
+      );  
+  }else{// API cache
+      event.respondWith(
+        caches.open("Api-cache").then(function(cache){
+          return fetch(request).then(function(apiRespone){
+            cache.put(request, apiRespone.clone())
+            return apiRespone
+          })
+        }).catch(function(){
+          return caches.match(request).then(function(){
+            if(response) return response
+              return caches.match('/fallback.json')
+          })
+        })
+      );
+  }
 });
